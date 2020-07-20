b4068fda1ab45de526a6688d2dd3c7b7c7297414
Vava Heirro <vavaheirro@live.com>
2020-07-20 11:30:17 +0700

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from "./screens/Home";
+import Downloads from "./screens/Downloads";
 import DriveHelp from "./screens/DriveHelp";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={Downloads} />
         <Route exact path="/download">
           <Home tab="downloads" />
         </Route>
