474c9d6f35b198ba438a3100db3f85072d369a42
Vava Heirro <vavaheirro@live.com>
2020-07-20 11:33:43 +0700

    Update TopNav.js

--- web/src/components/TopNav.js
+++ web/src/components/TopNav.js
@@ -7,6 +7,8 @@ export default function TopNav({ nav }) {
       <div className="content">
         <ul className="d-flex align-items-center space-around width-100 m-0">
           <li className={`cursor-pointer p-0 ph-1 height-100 d-flex align-items-center${nav === "pages" ? " border-bottom-1" : ""}`}>
+         <Link to="/search" className="height-100 d-flex align-items-center">
+            </Link>
           </li>
           <li className={`cursor-pointer p-0 ph-1 height-100 d-flex align-items-center${nav === "downloads" ? " border-bottom-1" : ""}`}>
             <Link to="/download" className="height-100 d-flex align-items-center">
@@ -17,6 +19,12 @@ export default function TopNav({ nav }) {
             </Link>
           </li>
           <li className={`cursor-pointer p-0 ph-1 height-100 d-flex align-items-center${nav === "drive" ? " border-bottom-1" : ""}`}>
+        <Link to="/drive" className="height-100 d-flex align-items-center">
+              <i className="h2 m-0 d-flex align-items-center">
+                <ion-icon name="push-outline" />
+              </i>
+              <span className="tablet-desktop-only ml-05">Drive</span>
+            </Link>
           </li>
         </ul>
       </div>
